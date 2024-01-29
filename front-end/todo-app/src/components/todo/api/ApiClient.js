88f55fb9ba8bb7ed5edacf6464ef7fc19bd569ec
Peerawichaya27 <pprw.27@gmail.com>
2024-01-30 00:49:39 +0700

    commit

--- front-end/todo-app/src/components/todo/api/ApiClient.js
+++ front-end/todo-app/src/components/todo/api/ApiClient.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 export const apiClient = axios.create(
     {
-        baseURL: 'http://localhost:8080'
+        //baseURL: 'http://localhost:5000'
+        baseURL: 'http://rest-api-full-stack-h2-env.eba-dmxcj99p.ap-southeast-2.elasticbeanstalk.com/'
     }
 )
